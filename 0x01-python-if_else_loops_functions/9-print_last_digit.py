816dc44f4de05c345b83a5bae772d01ebf8b2ada
osundwajeff <mulamajeff@tuta.io>
2022-05-31 15:27:47 -0700

    9x-print_last_digit.py file correctio

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,7 @@ def print_last_digit(number):
         last_digit = number % 10
     else:
         last_digit = number % -10
+        last_digit *= -1 
 
     print("{:d}".format(last_digit), end='')
     return (last_digit)
