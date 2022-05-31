83a36933da29e2251dd9646ef398da21ce213043
osundwajeff <mulamajeff@tuta.io>
2022-05-31 16:16:00 -0700

    13 singly linked lists

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * insert_node - inserts a node into a sorted singly linked list
+ * @head: head of the list
+ * @number: integer
+ * Return: address of new node, or NULL if failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *a;
+	listint_t *bef;
+
+	a = *head;
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	while (a != NULL)
+	{
+		if (a->n > number)
+			break;
+		bef = h;
+		a = a->next;
+	}
+
+	new->n = number;
+
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = a;
+		if (a == *head)
+			*head = new;
+		else
+			bef->next = new;
+	}
+
+	return (new);
+}
