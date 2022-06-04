703eb20fc803a0580d4b2c326dc0a4926a6121be
osundwajeff <mulamajeff@tuta.io>
2022-06-04 05:47:53 -0700

    added 1-element_at.py file

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    num = len(my_list)
+    if idx < 0:
+        return None
+    elif idx > num:
+        return None
+    else:
+        return my_list[idx]
