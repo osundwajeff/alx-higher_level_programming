98b1c1fd7edafcba390c9825ee7ccf8d5ea813f0
osundwajeff <mulamajeff@tuta.io>
2022-06-04 05:50:17 -0700

    minor fix 1-element_at.py file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     num = len(my_list)
     if idx < 0:
         return None
-    elif idx > num:
+    elif idx >= num:
         return None
     else:
         return my_list[idx]
