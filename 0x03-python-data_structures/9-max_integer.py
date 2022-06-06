b08f567d1b5a80ca5ddbdb34dc71e91c383eb092
osundwajeff <mulamajeff@tuta.io>
2022-06-06 09:06:05 -0700

    corrections

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == "":
-        return None
+    if len(my_list) == 0:
+        return (None)
 
     biggest = my_list[0]
     for x in range(len(my_list)):
