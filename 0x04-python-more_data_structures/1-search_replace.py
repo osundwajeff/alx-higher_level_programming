218dc7c9e6717adbc0757e4f2079220a1324276a
osundwajeff <mulamajeff@tuta.io>
2022-06-08 09:33:00 -0700

    fix whitespace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    x = [i if i !=search else replace for i in my_list]
+    x = [i if i != search else replace for i in my_list]
     return x
