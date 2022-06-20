1a377929e2dc4b7a6522dd63d5db8114ea0e2e3d
osundwajeff <mulamajeff@tuta.io>
2022-06-20 09:14:35 -0700

    added multiple files

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         div = None
     finally:
-        print("Inside results: {}".format(div))
+        print("Inside result: {}".format(div))
     return (div)
