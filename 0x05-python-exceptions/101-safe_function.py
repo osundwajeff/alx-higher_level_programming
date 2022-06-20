2ecc2b738cdd97b79a516a311b63016e9d682239
osundwajeff <mulamajeff@tuta.io>
2022-06-20 09:36:39 -0700

    advanced task 2

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except (ValueError):
+    except (BaseException):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
