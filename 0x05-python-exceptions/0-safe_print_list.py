ef4936a53a835a53f708cb00eee7468729939536
osundwajeff <mulamajeff@tuta.io>
2022-06-20 08:29:54 -0700

    changes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         while lst < x:
             print("{}".format(my_list[i], end = "")
             lst += 1
-            print("")
-            return (lst)
+        print("")
+        return (lst)
     except IndexError:
         print("")
         return (lst)
