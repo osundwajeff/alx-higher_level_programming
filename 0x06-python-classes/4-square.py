5e0012a7ff7aad124b491be066fd09de9b56d1f5
osundwajeff <mulamajeff@tuta.io>
2022-06-22 08:55:33 -0700

    corrections

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
     @size.setter
     def size(self, value):
         """Sets the size"""
-        if type(value) != int):
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
