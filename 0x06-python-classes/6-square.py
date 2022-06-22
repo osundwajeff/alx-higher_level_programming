5c319e00dec3dd2d475b407184730a0429066326
osundwajeff <mulamajeff@tuta.io>
2022-06-22 09:04:03 -0700

    corrections

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,6 +54,6 @@ class Square:
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
+            [print("#", end="") for j in range(self.__position)]
             [print("#", end="") for k in range(self.__size)]
             print("")
