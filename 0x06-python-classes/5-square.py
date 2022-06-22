1d518f963ce544fa887fef4bb858e7e4ed762523
osundwajeff <mulamajeff@tuta.io>
2022-06-22 08:18:08 -0700

    changes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 
 
-"""Define class Square"""
+"""Define class Square."""
 
 
 class Square:
-    """represent a square"""
+    """represent a square."""
     def __init__(self, size=0):
-        """initilize square
-        Args: size (int):size of new square"""
+        """initilize square.
+        Args:
+            size (int):size of new square.
+        """
         self.__size = size
 
     @property
     def size(self):
-        """Gets the current size"""
+        """Gets the current size."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Sets the size"""
+        """Sets the size."""
         if not isintance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -26,12 +28,12 @@ class Square:
         self.__size = value
 
     def area(self):
-        """get area of square"""
+        """get area of square."""
         area = self.__size * self.__size
         return (area)
 
     def my_print(self):
-        """ prints in stdout the square with the character #"""
+        """ prints in stdout the square with the character #."""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
