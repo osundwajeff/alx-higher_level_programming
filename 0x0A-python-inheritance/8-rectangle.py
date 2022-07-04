5b24ceba0caa938e98bb43a445866c51c722cd71
osundwajeff <mulamajeff@tuta.io>
2022-07-04 09:43:18 -0700

    8 & 9 rectangle files

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" class Rectangle that inherits from class BaseGeomtery. """
+BaseGeometry = __import__('7-base_geomtery').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle inherits from BaseGeometry """
+
+    def __init__(self, width, height):
+        """ Initialize a Rectangle.
+        Args:
+            width:(int): width of new Rectangle
+            height: (int): height of new Rectangle.
+        """
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" class Rectangle that inherits from class BaseGeomtery. """
+BaseGeometry = __import__('7-base_geomtery').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle inherits from BaseGeometry """
+
+    def __init__(self, width, height):
+        """ Initialize a Rectangle.
+        Args:
+            width:(int): width of new Rectangle
+            height: (int): height of new Rectangle.
+        """
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ return area of Rectangle """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ Return the print() and str() representation of Rectangle """
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
