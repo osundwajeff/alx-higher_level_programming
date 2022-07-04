ce7205e1a288bdbcf42b60838dd79d274e2f8413
osundwajeff <mulamajeff@tuta.io>
2022-07-04 08:52:33 -0700

    6-base_geometry file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class BaseGeometry """
+
+
+class BaseGeometry:
+    """ function area """
+
+    def area(self):
+        """ not implemented """
+
+        raise Exception("area() is not implemented")
