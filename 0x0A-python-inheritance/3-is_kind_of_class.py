122ec3c5606897e51a8ed3380641c5042905ccb7
osundwajeff <mulamajeff@tuta.io>
2022-07-04 08:31:05 -0700

    added 3-is kind of class file

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" is kind of class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ functions that returns True if
+    object is an instance of, or if 
+    object is an istance of a class 
+    that inherited from,specified class,
+    otherwise False"""
+
+    if obj isinstance(a_class):
+        return True
+    else:
+        return False
