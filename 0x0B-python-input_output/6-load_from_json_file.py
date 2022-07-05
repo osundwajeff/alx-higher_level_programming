720f112a5949603e5b028d0e6c7cac1941df56b1
osundwajeff <mulamajeff@tuta.io>
2022-07-05 08:09:43 -0700

    json-py operations

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" IO functions """
+from json import JSONDecoder
+
+
+def load_from_json_file(filename):
+    """ Creates an object from its JSON representation stored in a file.
+    Args:
+        filename (str): The name of the file containing the JSON string.
+    Returns:
+        any: An object corresponding to the JSON string in the file,
+        else an exception is thrown."""
+    lines = []
+    with open(filename, encodings="utf-8") as f:
+        for line in f.read.lines():
+            lines.append(line)
+    return JSONDecoder().decode("".join(lines))
