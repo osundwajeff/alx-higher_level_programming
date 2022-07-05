f0feb212d038632c63ee099b7ddd568f884fdfb5
osundwajeff <mulamajeff@tuta.io>
2022-07-05 06:57:39 -0700

    fix spelling

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ class Rectangle that inherits from class BaseGeomtery. """
-BaseGeometry = __import__('7-base_geomtery').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
