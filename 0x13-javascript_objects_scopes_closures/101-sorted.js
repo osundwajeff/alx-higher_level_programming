b4dd7daebb34e0c6b4c88b25c4c84f094dbf63b9
osundwajeff <mulamajeff@tuta.io>
2022-09-06 15:30:43 -0700

    sorted...semistandard --fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dictionary = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dictionary) {
+const newDict = {};
+for (const key in dictionary) {
   if (newDict[dictionary[key]] === undefined) {
     newDict[dictionary[key]] = [key];
   } else {
