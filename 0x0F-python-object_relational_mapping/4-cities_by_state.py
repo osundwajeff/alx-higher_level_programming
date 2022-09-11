c4a9701da15168b028397223a8bc7330207247b1
osundwajeff <mulamajeff@tuta.io>
2022-09-11 03:26:27 -0700

    cities by state

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" list all cities from database hbtn_0e_0_usa.
+    should be in ascending order by cities.id.
+    should take three arguments:
+        ./2-my_filter_states.py username,
+        password,
+        database name.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+            FROM `cities` as `c` \
+            INNER JOIN `states` as `s` \
+            ON `c`.`state_id` = `s`.`id` \
+            ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
