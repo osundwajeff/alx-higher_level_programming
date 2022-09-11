89c25412b5c88a8c5efeef19a8f9ea9521fe57cc
osundwajeff <mulamajeff@tuta.io>
2022-09-11 03:39:13 -0700

    fix length of line

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
             INNER JOIN `states` as `s` \
             ON `c`.`state_id` = `s`.`id` \
             ORDER BY `c`.`id`")
-    print(", ".join([city[2] for city in c.fetchall() if city[4] == sys.argv[4]]))
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
